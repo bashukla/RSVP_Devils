69f7989c3fd2e3587f53ca181e461e388fb5df2e
Conner-Black-cb <cblack20@asu.edu>
2026-02-25 14:07:31 -0700

    Database queury
    Database query, package files

--- user_table_ddl.sql
+++ user_table_ddl.sql
@@ -1,4 +1,16 @@
 CREATE TABLE user (
     email VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    user_id int AUTO_INCREMENT UNIQUE
+);
+
+CREATE TABLE events (
+    event_id INT AUTO_INCREMENT PRIMARY KEY,
+    type VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    event_datetime DATETIME NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    created_by INT NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES user(user_id) 
+
 );
