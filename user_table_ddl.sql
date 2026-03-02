162871cd8c2720181b0dfa8eb33497996f3b435b
Conner-Black-cb <cblack20@asu.edu>
2026-03-02 12:17:39 -0700

    Update user_table_ddl.sql

--- user_table_ddl.sql
+++ user_table_ddl.sql
@@ -1,16 +1,26 @@
-CREATE TABLE user (
-    email VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
-    user_id int AUTO_INCREMENT UNIQUE
-);
+-- CREATE TABLE user (
+--     email VARCHAR(255) PRIMARY KEY,
+--     password VARCHAR(255) NOT NULL,
+--     user_id int AUTO_INCREMENT UNIQUE
+-- );
 
-CREATE TABLE events (
-    event_id INT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    event_datetime DATETIME NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    created_by INT NOT NULL,
-    FOREIGN KEY (created_by) REFERENCES user(user_id) 
+-- CREATE TABLE events (
+--     event_id INT AUTO_INCREMENT PRIMARY KEY,
+--     type VARCHAR(255) NOT NULL,
+--     description VARCHAR(255),
+--     event_datetime DATETIME NOT NULL,
+--     location VARCHAR(255) NOT NULL,
+--     created_by INT NOT NULL,
+--     FOREIGN KEY (created_by) REFERENCES user(user_id) 
 
-);
+-- );
+
+-- CREATE TABLE registration (
+--     regId INT AUTO_INCREMENT PRIMARY KEY,
+--     user_id INT NOT NULL,
+--     event_id INT NOT NULL,
+--     reg_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (user_id) REFERENCES user(user_id),
+--     FOREIGN KEY (event_id) REFERENCES events(event_id),
+--     UNIQUE (user_id,event_id)
+-- );
